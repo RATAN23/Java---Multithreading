6ff38bb2aae79542ac7c039409351df4261ff812
RATAN23 <rnghasti42@gmail.com>
2025-04-08 01:49:41 +0530

    added client.java

--- multithread/Client.java
+++ multithread/Client.java
@@ -1,6 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
-    
+
+    public Runnable getRunnable() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                int port = 8081;
+                try {
+                    InetAddress adrress = InetAddress.getByName("localhost");
+                    Socket socket = new Socket(adrress, port);
+                    try {
+                        PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
+                        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        toSocket.println("Hello from the client");
+                        String line = fromSocket.readLine();
+                        System.out.println("Response from the server = " + line);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        };
+    }
+
     public static void main(String[] args) {
-        
+        Client client = new Client();
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread thread = new Thread(client.getRunnable());
+                thread.start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
